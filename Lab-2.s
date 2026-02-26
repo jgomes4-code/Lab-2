d2c90f6c3b30230adfbadbd5074db5e05866837c
jgomes4-code <jgomes4@umbc.edu>
2026-02-26 17:06:42 +0000

    deleted compiled, edited assembly code

--- Lab-2.s
+++ Lab-2.s
@@ -1,36 +1,31 @@
-# hamming_convert.s  (AT&T syntax for gcc)
-# int hamming_convert(char *str1, char *str2);
-# args: str1 in RDI, str2 in RSI
-# return: EAX
-
-    .text
+     .text
     .globl hamming_convert
     .type  hamming_convert, @function
 
 hamming_convert:
-    xorl %eax, %eax          # total = 0  (EAX will be our running Hamming distance)
+    xorl %eax, %eax         
 
 .L_outer:
-    movzbl (%rdi), %edx      # EDX = (unsigned char)*str1
-    movzbl (%rsi), %ecx      # ECX = (unsigned char)*str2
+    movzbl (%rdi), %edx      
+    movzbl (%rsi), %ecx      
 
-    testb %dl, %dl           # if *str1 == 0 -> done
+    testb %dl, %dl           
     je .L_done
-    testb %cl, %cl           # if *str2 == 0 -> done
+    testb %cl, %cl           
     je .L_done
 
-    xorb %cl, %dl            # DL = DL XOR CL  (bits that differ become 1)
+    xorb %cl, %dl           
 
-    movl $8, %ecx            # bit loop counter = 8 bits
+    movl $8, %ecx            
 
 .L_bits:
-    shrb $1, %dl             # shift right, LSB goes into CF
-    adcl $0, %eax            # EAX += CF
+    shrb $1, %dl             
+    adcl $0, %eax            
     decl %ecx
     jne .L_bits
 
-    incq %rdi                # str1++
-    incq %rsi                # str2++
+    incq %rdi               
+    incq %rsi                
     jmp .L_outer
 
 .L_done:
