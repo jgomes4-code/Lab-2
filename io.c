634f4681741b2ca4bf750f0c625a636d7926d94e
jgomes4-code <jgomes4@umbc.edu>
2026-02-26 16:28:57 +0000

    updated io and recomplied

--- io.c
+++ io.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 extern int hamming_convert(char *str1, char *str2);
 
+static void strip_newline(char *s) {
+    s[strcspn(s, "\n")] = '\0';
+}
+
 int main(void) {
-    char *str1 = NULL;
-    char *str2 = NULL;
-    size_t len1 = 0;
-    size_t len2 = 0;
+    char str1[256];
+    char str2[256];
 
     printf("Enter String 1:\n");
-    getline(&str1, &len1, stdin);
+    if (!fgets(str1, sizeof str1, stdin)) return 1;
+    strip_newline(str1);
 
     printf("Enter String 2:\n");
-    getline(&str2, &len2, stdin);
-
-    // remove newline characters
-    str1[strcspn(str1, "\n")] = '\0';
-    str2[strcspn(str2, "\n")] = '\0';
+    if (!fgets(str2, sizeof str2, stdin)) return 1;
+    strip_newline(str2);
 
     int result = hamming_convert(str1, str2);
     printf("The Hamming Distance Between the Two Strings Are: %d\n", result);
-
-    free(str1);
-    free(str2);
 
     return 0;
 }
